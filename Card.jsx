8883b80defe78aa015f87a5b46289ef31225a9fc
Hangwe <hangwe00@gmail.com>
2025-04-10 23:21:18 +0200

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,11 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>HRN</h2>
+            <p>I make CV's and play cars</p>
+
+        </div>
+    );
+}
