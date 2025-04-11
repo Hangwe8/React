b274ea93520deec2fa8e1110edef26411e8582bf
Hangwe <hangwe00@gmail.com>
2025-04-11 23:42:11 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,11 +2,11 @@ import Card from './Card.jsx'
 
 function App() {
   return(
-    <Card></Card>
+    <Card/>
 
   );
 
 }
 
 
-export default App
+export default App;
--- Card.jsx
+++ Card.jsx
@@ -1,11 +1,14 @@
+import profilePic from './assets/Profile.jpg'
 
 function Card(){
     return(
         <div className="card">
-            <img></img>
+            <img src={profilePic}alt="profile picture"></img>
             <h2>HRN</h2>
             <p>I make CV's and play cars</p>
 
         </div>
     );
 }
+
+export default Card;
